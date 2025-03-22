93b134eb5ff5681e2d01d3263a4d6596e8a3da5a
Niranjan Shah <niranjanshah474@gmail.com>
2025-03-22 22:55:55 +1100

    sidebar items added, need to complete it with icoons and hrefs

--- app/(dashboard)/_components/sidebar-item.tsx
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -1,6 +1,8 @@
 "use client"
 
+import { cn } from "@/lib/utils";
 import { LucideIcon } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
 
 interface SidebarItemProps{
     icon: LucideIcon;
@@ -9,9 +11,32 @@ interface SidebarItemProps{
 }
 
 const SidebarItem = ({icon,label, href}:SidebarItemProps) => {
-    return ( <div>
-        Sidebar
-    </div> );
+
+    const pathname = usePathname();
+    const router = useRouter();
+
+    const isActive = (pathname === "/" && href=="/") ||
+                        pathname === href ||
+                        pathname?.startsWith(`${href}/`)
+
+    const onClick = () => {
+        router.push(href)
+    }
+    return ( 
+    <button 
+    onClick={onClick}
+    type="button"
+    className={cn(`flex items-center gap-x-2 text-zinc-500 
+        text-sm font-[500] pl-6 transition-all hover:text-zinc-600
+        hover:bg-zinc-300/20`,
+    isActive && "text-sky-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700"
+    )
+    }
+    >
+        <div className="flex items-center gap-x-2 py-4">
+            {label}
+        </div>
+    </button>);
 }
  
 export default SidebarItem
