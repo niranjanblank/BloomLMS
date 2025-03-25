89d92e019dac17b06d58600ac572234ffac37d8c
Niranjan Shah <niranjanshah474@gmail.com>
2025-03-25 11:22:10 +1100

    form and route for saving chapter title added

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/ChaptersForm.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/ChaptersForm.tsx
@@ -13,7 +13,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
-import { Pencil, PlusCircle } from "lucide-react";
+import { Loader2, Pencil, PlusCircle } from "lucide-react";
 import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import toast from "react-hot-toast";
@@ -80,8 +80,19 @@ const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
       setIsUpdating(false)
     }
   }
+
+  const onEdit = (id: string)=> {
+    router.push(`/teacher/courses/${courseId}/chapters/${id}`)
+  }
   return (
-    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+    <div className="relative mt-6 border bg-slate-100 rounded-md p-4">
+      {/* to block other updates when reordering */}
+      {isUpdating && (
+        <div className="absolute h-full w-full bg-slate-500/20 top-0 right-0 
+        rounded-m flex items-center justify-center">
+          <Loader2 className="animate-spin h-6 w-6 text-sky-700"/>
+          </div>
+      )}
       <div className="font-medium flex items-center justify-between">
         Course Chapters
         <Button variant="ghost" onClick={toggleCreating}>
@@ -140,7 +151,7 @@ const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
           {/* all a list of chapters */}
 
           <ChaptersList 
-            onEdit = {()=> {}}
+            onEdit = {onEdit}
             onReorder = {onReorder}
             items={initialData.chapters || []}
           />
